78c6eb2edf88a5bb0bedbf32f9e9d976b31a1e7c
nouhmoud <nouhaila.ajaamoud1996@gmail.com>
2025-10-19 00:00:53 +0100

    commit2

---
+++ src/main/java/entities/Category.java
@@ -0,0 +1,32 @@
+package entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products;
+
+    public Category() {}
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public List<Product> getProducts() { return products; }
+    public void setProducts(List<Product> products) { this.products = products; }
+}
